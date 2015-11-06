de9cbe94375a81e31bed56f5c049680e1be9bd84
hayashidesu <katsuhiko.hayashi.847@gmail.com>
2015-11-06 14:37:15 +0900

    no message

--- CollectionViewSample/CustomCell.swift
+++ CollectionViewSample/CustomCell.swift
@@ -16,7 +16,7 @@ class CustomCell: UICollectionViewCell {
     override init(frame: CGRect){
         super.init(frame: frame)
     }
-    required init(coder aDecoder: NSCoder){
+    required init?(coder aDecoder: NSCoder){
         super.init(coder: aDecoder)
     }
 }
